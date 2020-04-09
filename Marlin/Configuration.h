8868fb8d97932d649364a94292878bd92d76e768
fortes23 <brais.fortes@gmail.com>
2020-04-09 14:06:26 +0200

    conf: Steps per unit

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -740,7 +740,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {79.364716,79.17719,4038.650404,432.1728}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.05,79.17719,4014.56,458.7}
 
 /**
  * Default Max Feed Rate (mm/s)

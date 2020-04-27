04e4e8e1a0873ced2eb096d338cb7d8270aaa390
Brais Fortes <brais.fortes@gmail.com>
2020-04-27 17:57:57 +0200

    conf: Increment X_BED_SIZE

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1102,7 +1102,7 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 130
+#define X_BED_SIZE 145
 #define Y_BED_SIZE 179
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
